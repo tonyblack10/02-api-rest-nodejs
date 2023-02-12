684ec33e6a395a050592100a155337faabd33935
Tony Costa <tonyblack10@gmail.com>
2023-02-12 03:09:54 -0300

    testando criacao de transacao

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,22 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('teste de exemplo', () => {
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toEqual(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('teste de exemplo', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
