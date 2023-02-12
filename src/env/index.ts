b4fa50f162e0d07c9f3c760f7648b0e5ae175358
Tony Costa <tonyblack10@gmail.com>
2023-02-12 03:33:20 -0300

    configurando banco de testes

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,11 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
@@ -10,9 +16,9 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error('Invalid env variables!', _env.error.format())
+  console.error('⚠️ Invalid environment variables', _env.error.format())
 
-  throw new Error('Invalid env variable')
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
